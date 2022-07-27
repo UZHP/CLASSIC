e0bb680b6aa25d5b2c201846f87dd022ed04ae34
Youssef Tamer <107003202+UZHP@users.noreply.github.com>
2022-07-27 20:09:39 +0300

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -27,10 +27,55 @@ $(function () {
     //     $(this).css('paddingTop', ($(window).height() - $('.slider li').height()) / 2);
     // });
 
+    $('.allTestimonials .slider').bxSlider({
+        pager: false,
+        speed: 1000,
+        auto: true,
+        controls: false,
+        // mode: 'vertical',
+        // easing: 'linear',
+        randomStart: true,
+        // ticker: true,
+        // tickerHover: true
+    });
+    
     slider.bxSlider({
-        // infiniteLoop: false
+        // infiniteLoop: false,
+        // hideControlOnEnd: true,
         pager: true,
         speed: 1000,
+        auto: true,
     });
+
+    var mixer = mixitup('.products');
+
+    $('.productsButtons li').click(function () {
+        $(this).addClass('selected').siblings().removeClass('selected')
+    })
+
+    let spanTop = document.querySelector(".top");
+    window.onscroll = function () {
+        // console.log(this.scrollY);
+        if (this.scrollY >= 625) {
+            spanTop.classList.add("show");
+        } else {
+            spanTop.classList.remove("show");
+        };
+    };
+
+    spanTop.onclick = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    var loader = document.querySelector(".loader");
+
+    window.addEventListener("load", vanish);
+
+    function vanish() {
+        loader.classList.add("disappear")
+    }
 
 });
